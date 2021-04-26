f3a94a061fe00600eafff42144da588b1ff972ce
JShawnDunn <jshawndunn@gmail.com>
2021-04-26 15:35:28 -0400

    add reset and quit button functionality

--- js/main.js
+++ js/main.js
@@ -8,29 +8,32 @@ player2.setAttribute("onclick", 'runGame(this)')
 
 
 
-function runGame(player){
+function runGame(){
     let currentQuote = document.querySelector(".game__current-quote");
     let playerInput = document.querySelector(".game__player-input");
     playerInput.value = ""
     
-    function madeIt(){
-        console.log(`${this.value}`)
-    }
     
     gameScreen.style.display = "block";
     welcomeScreen.style.display = "none";
     
-    const data = fetch('https://thesimpsonsquoteapi.glitch.me/quotes?count=10')
+    fetch('https://thesimpsonsquoteapi.glitch.me/quotes?count=10')
     .then(function(responseData){
         return responseData.json()
     })
     .then(function(jsonData){
         currentQuote.innerText = jsonData[0]['quote']
         playerInput.addEventListener("keyup", madeIt)
-        
-        return jsonData
-    })
-    .catch(function(error){
-        console.log(`${error} has occured.`)
-    })
+        let quit = document.querySelector(".game__quit")
+        quit.addEventListener("click", ()=>window.location.reload())
+        let restart = document.querySelector(".game__restart")
+        restart.addEventListener("click", runGame)
+
+        function madeIt(){
+            console.log(`${this.value}`)
+        }
+        })
+        .catch(function(error){
+            console.log(`${error} has occured.`)
+        })
 }
