e2ee15b13663e0001983c274b8939903968f10ce
JShawnDunn <jshawndunn@gmail.com>
2021-04-26 15:17:11 -0400

    ppadd player button functionality. add JS logic  to grab elements.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,36 @@
-fetch('https://thesimpsonsquoteapi.glitch.me/quotes?count=10')
+let player1 = document.querySelector(".welcome__player1");
+let player2 = document.querySelector(".welcome__player2");
+let welcomeScreen = document.querySelector(".welcome");
+let gameScreen = document.querySelector('.game');
+
+player1.setAttribute("onclick", 'runGame(this)')
+player2.setAttribute("onclick", 'runGame(this)')
+
+
+
+function runGame(player){
+    let currentQuote = document.querySelector(".game__current-quote");
+    let playerInput = document.querySelector(".game__player-input");
+    playerInput.value = ""
+    
+    function madeIt(){
+        console.log(`${this.value}`)
+    }
+    
+    gameScreen.style.display = "block";
+    welcomeScreen.style.display = "none";
+    
+    const data = fetch('https://thesimpsonsquoteapi.glitch.me/quotes?count=10')
     .then(function(responseData){
         return responseData.json()
     })
     .then(function(jsonData){
-        console.log(jsonData)
+        currentQuote.innerText = jsonData[0]['quote']
+        playerInput.addEventListener("keyup", madeIt)
+        
+        return jsonData
     })
+    .catch(function(error){
+        console.log(`${error} has occured.`)
+    })
+}
