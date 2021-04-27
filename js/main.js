a9d77f82d5a6e257307df2d67e0724e60e6d858d
JShawnDunn <jshawndunn@gmail.com>
2021-04-27 16:17:43 -0400

    add css. refactor JS fetch.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 //grab needed elements
-let player1 = document.querySelector(".welcome__player1");
-let player2 = document.querySelector(".welcome__player2");
+let onePlayer = document.querySelector(".welcome__one-player");
+let twoPlayer = document.querySelector(".welcome__two-player");
 let welcomeScreen = document.querySelector(".welcome");
 let gameScreen = document.querySelector('.game');
 
 //add click event to buttons to start game
-player1.setAttribute("onclick", 'runGame()')
-player2.setAttribute("onclick", 'runGame()')
+onePlayer.setAttribute("onclick", 'runGame()')
+twoPlayer.setAttribute("onclick", 'runGame()')
 
 
 //main game loop
@@ -16,23 +16,31 @@ function runGame(){
     let playerInput = document.querySelector(".game__player-input");
     //set current player input to an empty string
     playerInput.value = ""
-    gameScreen.style.display = "block";
+    gameScreen.style.display = "flex";
     welcomeScreen.style.display = "none";
     //array to hold results of typed quotes
     let playerOneOutput = [];
     let playerOnePosition = 0;
 
+    //grab data
     fetch('https://thesimpsonsquoteapi.glitch.me/quotes?count=10')
     .then(function(responseData){
         return responseData.json()
     })
     .then(function(jsonData){
+        //print current quote to screen
         currentQuote.innerText = jsonData[playerOnePosition]['quote']
+        //quit button
         let quit = document.querySelector(".game__quit")
         quit.addEventListener("click", ()=>window.location.reload())
+        //restart button
         let restart = document.querySelector(".game__restart")
-        restart.addEventListener("click", runGame)
+        restart.addEventListener("click", ()=>{
+            playerOneOutput = []
+            playerOnePosition = 0
+        })
 
+        //actions when player hit enter
         playerInput.addEventListener('keyup', function(event) {
             if (event.code === 'Enter') {
                 playerOneOutput.push(playerInput.value)
@@ -43,7 +51,7 @@ function runGame(){
             }
         })
         })
-        .catch(function(error){
-            console.log(`${error} has occured.`)
-        })
+    .catch(function(error){
+        console.log(`${error} has occured.`)
+    })
 }
