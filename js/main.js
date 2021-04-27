89b16b056b7f86780cde4c3c3f99b28d929053c1
JShawnDunn <jshawndunn@gmail.com>
2021-04-27 12:04:22 -0400

    add functionality to capture user input, clear user input field and move to the next quote

--- js/main.js
+++ js/main.js
@@ -18,22 +18,30 @@ function runGame(){
     playerInput.value = ""
     gameScreen.style.display = "block";
     welcomeScreen.style.display = "none";
-    
+    //array to hold results of typed quotes
+    let playerOneOutput = [];
+    let playerOnePosition = 0;
+
     fetch('https://thesimpsonsquoteapi.glitch.me/quotes?count=10')
     .then(function(responseData){
         return responseData.json()
     })
     .then(function(jsonData){
-        currentQuote.innerText = jsonData[0]['quote']
-        playerInput.addEventListener("keyup", madeIt)
+        currentQuote.innerText = jsonData[playerOnePosition]['quote']
         let quit = document.querySelector(".game__quit")
         quit.addEventListener("click", ()=>window.location.reload())
         let restart = document.querySelector(".game__restart")
         restart.addEventListener("click", runGame)
 
-        function madeIt(){
-            console.log(`${this.value}`)
-        }
+        playerInput.addEventListener('keyup', function(event) {
+            if (event.code === 'Enter') {
+                playerOneOutput.push(playerInput.value)
+                console.log(playerOneOutput)
+                playerInput.value = ""
+                playerOnePosition++
+                currentQuote.innerText = jsonData[playerOnePosition]['quote']
+            }
+        })
         })
         .catch(function(error){
             console.log(`${error} has occured.`)
